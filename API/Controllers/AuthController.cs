d907f01347c23ee2a6b5f3be980c90937a0d6c37
Ahmad Abdullah <ahmadabdullah.formal@gmail.com>
2026-09-11 00:12:27 +0300

    feat(API): Add login and logout controller

---
+++ API/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class AuthController : BaseApiController
+{
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult> Login([FromBody] LoginDto dto)
+    {
+        var result = await _authService.LoginAsync(dto);
+        return HandleResult(result);
+    }
+    [Authorize]
+    [HttpPost("logout")]
+    public async Task<ActionResult> Logout()
+    {
+        var result = await _authService.LogoutAsync();
+        return HandleResult(result);
+    }
+}
--- API/GlobalUsings.cs
+++ API/GlobalUsings.cs
@@ -1,2 +1,4 @@
 ﻿global using Infrastructure;
 global using Application.Common;
+global using Application.Interfaces;
+global using Application.Dtos;
