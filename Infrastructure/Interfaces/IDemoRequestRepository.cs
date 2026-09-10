fe765a687b8dd9465417c418e369389d37ca72df
Ahmad Abdullah <ahmadabdullah.formal@gmail.com>
2026-09-10 22:09:56 +0300

    feat(Domain): Add DemoRequest Repository and Interface

---
+++ Infrastructure/Interfaces/IDemoRequestRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Infrastructure.Interfaces;
+
+public interface IDemoRequestRepository : IRepository<DemoRequest>
+{
+    Task<List<DemoRequest>> GetByStatusAsync(DemoRequestStatus status);
+    Task UpdateStatusAsync(Guid id, DemoRequestStatus status);
+}
---
+++ Infrastructure/Repositories/DemoRequestRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class DemoRequestRepository : Repository<DemoRequest>, IDemoRequestRepository
+{
+    public DemoRequestRepository(AppDbContext db) : base(db) { }
+
+    public async Task<List<DemoRequest>> GetByStatusAsync(DemoRequestStatus status) =>
+        await DbSet.Where(x => x.Status == status)
+            .OrderByDescending(x => x.CreatedAtUtc)
+            .ToListAsync();
+
+    public async Task UpdateStatusAsync(Guid id, DemoRequestStatus status)
+    {
+        var entity = await DbSet.FindAsync(id)
+            ?? throw new KeyNotFoundException($"DemoRequest {id} not found");
+        entity.Status = status;
+        await SaveChangesAsync();
+    }
+}
