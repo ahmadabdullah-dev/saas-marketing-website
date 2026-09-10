83e5d97c22ef1227f53c19b3fe6a0c8e64c07f1f
Ahmad Abdullah <ahmadabdullah.formal@gmail.com>
2026-09-10 23:59:55 +0300

    feat(API): Add BaseApiController

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BaseApiController : ControllerBase
+{
+    protected ActionResult HandleResult<T>(Result<T> result)
+    {
+        if (result.IsSuccess && result.Value != null)
+            return Ok(result.Value);
+
+        if (result.IsSuccess && result.Value == null)
+            return NotFound();
+
+        return result.ErrorCode switch
+        {
+            400 => BadRequest(result.Error),
+            401 => Unauthorized(result.Error),
+            403 => StatusCode(403, result.Error),
+            404 => NotFound(result.Error),
+            409 => Conflict(result.Error),
+            422 => UnprocessableEntity(result.Error),
+            429 => StatusCode(429, result.Error),
+            _ => StatusCode(result.ErrorCode == 0 ? 500 : result.ErrorCode, result.Error)
+        };
+    }
+}
--- API/GlobalUsings.cs
+++ API/GlobalUsings.cs
@@ -1,1 +1,2 @@
 ﻿global using Infrastructure;
+global using Application.Common;
