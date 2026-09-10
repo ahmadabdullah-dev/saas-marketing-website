b21e199ebcb07f124677f70d23a2d2426ebf8309
Ahmad Abdullah <ahmadabdullah.formal@gmail.com>
2026-09-10 22:03:59 +0300

    feat(Domain): Add ContactSubmissionR epository and Interface

---
+++ Infrastructure/Interfaces/IContactSubmissionRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Infrastructure.Interfaces;
+
+public interface IContactSubmissionRepository : IRepository<ContactSubmission>
+{
+    Task<List<ContactSubmission>> GetByStatusAsync(ContactSubmissionStatus status);
+    Task<List<ContactSubmission>> GetByEmailAsync(string email);
+}
---
+++ Infrastructure/Repositories/ContactSubmissionRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class ContactSubmissionRepository : Repository<ContactSubmission>, IContactSubmissionRepository
+{
+    public ContactSubmissionRepository(AppDbContext db) : base(db) { }
+
+    public async Task<List<ContactSubmission>> GetByStatusAsync(ContactSubmissionStatus status) =>
+        await DbSet.Where(x => x.Status == status)
+            .OrderByDescending(x => x.CreatedAtUtc)
+            .ToListAsync();
+
+    public async Task<List<ContactSubmission>> GetByEmailAsync(string email) =>
+        await DbSet.Where(x => x.Email == email)
+            .OrderByDescending(x => x.CreatedAtUtc)
+            .ToListAsync();
+}
