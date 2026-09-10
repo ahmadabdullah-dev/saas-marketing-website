01ef68816d01ae55bf7afcac568cd2fc41fcfe0c
Ahmad Abdullah <ahmadabdullah.formal@gmail.com>
2026-09-10 20:58:09 +0300

    feat(Infrastructure): Add Repository and IRepository

---
+++ Infrastructure/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Infrastructure.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    Task<T?> GetByIdAsync(Guid id);
+    Task<List<T>> GetAllAsync();
+    Task AddAsync(T entity);
+    void Update(T entity);
+    void Remove(T entity);
+    Task<int> SaveChangesAsync();
+}   
---
+++ Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class Repository<T> : IRepository<T> where T : class
+{
+    protected readonly AppDbContext Db;
+    protected readonly DbSet<T> DbSet;
+
+    public Repository(AppDbContext db)
+    {
+        Db = db;
+        DbSet = db.Set<T>();
+    }
+
+    public async Task<T?> GetByIdAsync(Guid id) => await DbSet.FindAsync(id);
+
+    public async Task<List<T>> GetAllAsync() => await DbSet.ToListAsync();
+
+    public async Task AddAsync(T entity) => await DbSet.AddAsync(entity);
+
+    public void Update(T entity) => DbSet.Update(entity);
+
+    public void Remove(T entity) => DbSet.Remove(entity);
+
+    public Task<int> SaveChangesAsync() => Db.SaveChangesAsync();
+}
+
