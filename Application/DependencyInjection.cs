ca808686908082b33f6abb1e398edef346db2f87
Ahmad Abdullah <ahmadabdullah.formal@gmail.com>
2026-09-12 16:50:57 +0300

    feat(API:Application): Add EmailService

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -7,6 +7,8 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddScoped<IAuthService, AuthService>();
+        services.AddScoped<IEmailService, EmailService>();
+
         return services;
     }
 }
